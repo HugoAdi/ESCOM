880bddcf97211c6d7e9260aed73707da4e23ab92
Naho <hugoadijm@gmail.com>
2022-02-24 11:56:55 -0600

    Ejercicio 2 terminado, falta contar las operaciones de MCD

--- Analisis de Algoritmos/Practicas/Ejercicio_2.c
+++ Analisis de Algoritmos/Practicas/Ejercicio_2.c
@@ -4,5 +4,25 @@
 #include <math.h>
 
 int MCD(int a, int b){
-    
+    int aux;
+    while(b != 0){
+        aux = b;
+        b=a % b;
+        a= aux;
+    }
+    return a;
+}
+
+int fib(int n){
+    if(n<=1) return n;
+    return fib(n-1) + fib(n-2);
+}
+
+int main(){
+    int num_ordenadas, i;
+    num_ordenadas = 10;
+    for(i=0;i<num_ordenadas;i++){
+        MCD(fib(i),fib(i+1));
+    }
+    return 0;
 }
