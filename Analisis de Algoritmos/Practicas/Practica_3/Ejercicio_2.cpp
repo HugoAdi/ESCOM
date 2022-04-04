64790323e41820a7f6857fa4fe976014c30fb301
HugoAdi <hugoadijm@gmail.com>
2022-04-03 19:55:22 -0500

    si

--- Analisis de Algoritmos/Practicas/Practica_3/Ejercicio_2.cpp
+++ Analisis de Algoritmos/Practicas/Practica_3/Ejercicio_2.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-int busqueda(int inicio,int final,int n, int array[]){
+int busqueda_Recursiva(int inicio,int final,int n, int array[]){
     int i = inicio + ((final-inicio)/3);
     int j = inicio + (2 * ((final - inicio)/3));
     if(inicio > final){
         return -1;
     }
     else if(array[inicio] < n  && n < array[i]){
-        return busqueda(inicio,i,n,array);
+        return busqueda_Recursiva(inicio,i,n,array);
     }
     else if(array[i] < n && n < array[j]){
-        return busqueda(i,j,n,array);
+        return busqueda_Recursiva(i,j,n,array);
     }
     else if(array[j] < n && n < array[final]){
-        return busqueda(j,final,n,array);
+        return busqueda_Recursiva(j,final,n,array);
     }
     else if(array[inicio] == n){
         return inicio;
@@ -28,7 +28,45 @@ int busqueda(int inicio,int final,int n, int array[]){
     else if(array[j]==n){
         return j;
     }
-    return 0;
+    return -2;
+}
+int busqueda_Iterativa(int array[],int tam,int n){
+    int inicio = 0;
+    int final = tam;
+    int i;
+    int j;
+    while(true){
+        i = inicio + ((final-inicio)/3);
+        j = inicio + (2 * ((final - inicio)/3));
+        if(inicio > final){
+            return -1;
+        }
+        else if(array[i] < n && n < array[j]){
+            final = i;
+        }
+        else if(array[i] < n && n < array[j]){
+            inicio = i;
+            final = j;
+        }
+        else if(array[j] < n && n < array[final]){
+            inicio = j;
+        }
+         else if(array[inicio] == n){
+            return inicio;
+        }
+        else if(array[final] == n){
+            return final;
+        }
+        else if(array[i]==n){
+            return i;
+        }
+        else if(array[j]==n){
+            return j;
+        }
+        else
+            break;
+    }
+    return -2;
 }
 int main(){
     int array[12];
@@ -37,5 +75,5 @@ int main(){
         array[i] = i+1;
     cout<<"Ingresa el elemento a buscar:";
     cin>>elemento;
-    cout<<"El elemento esta en "<<endl<<busqueda(0,11,elemento,array)<<endl;
+    cout<<"El elemento esta en "<<endl<<busqueda_Iterativa(array,11,elemento)<<endl;
 }
